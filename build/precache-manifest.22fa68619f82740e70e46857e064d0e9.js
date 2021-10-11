efc0105a18835d781c67ad8a4d90b8bb92b61dc1
Hanii mustofa <32809284+hanmustofa@users.noreply.github.com>
2021-10-11 15:42:52 +0700

    Add files via upload
    "penambahan build"

---
+++ build/precache-manifest.22fa68619f82740e70e46857e064d0e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90d74dd2014da60261c5e5bba0df6e36",
+    "url": "/hanmustofa/my-SPA/tree/master/index.html"
+  },
+  {
+    "revision": "0ebab15f252320850582",
+    "url": "/hanmustofa/my-SPA/tree/master/static/css/main.c8c48d66.chunk.css"
+  },
+  {
+    "revision": "8ec59e957ae2017e2059",
+    "url": "/hanmustofa/my-SPA/tree/master/static/js/2.3f7b2753.chunk.js"
+  },
+  {
+    "revision": "3143274d7d26932c98894a52e75c487a",
+    "url": "/hanmustofa/my-SPA/tree/master/static/js/2.3f7b2753.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0ebab15f252320850582",
+    "url": "/hanmustofa/my-SPA/tree/master/static/js/main.474bd84a.chunk.js"
+  },
+  {
+    "revision": "c50edd3430da3398c402",
+    "url": "/hanmustofa/my-SPA/tree/master/static/js/runtime-main.10b0b2ab.js"
+  },
+  {
+    "revision": "6dfded41d745f3cdf7321000da39cd62",
+    "url": "/hanmustofa/my-SPA/tree/master/static/media/han.6dfded41.jpg"
+  }
+]);
